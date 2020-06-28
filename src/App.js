f88f76ef770a53362a3c077c561ef448bd78e4b1
arnab <arnab>
2020-06-28 15:16:44 +0530

    all things done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+  CloudsPre,
+  DaylightPre,
+  FlightPre,
+  GrayPre,
+  GreenPre,
+  CloudsFull,
+  DaylightFull,
+  FlightFull,
+  GrayFull,
+  GreenFull
+} from './assets/images';
 import './App.css';
 
+const images = [
+  {
+    pre: CloudsPre,
+    full: CloudsFull,
+    text: 'Clouds Daylight Forest Grass'
+  },
+  {
+    pre: DaylightPre,
+    full: DaylightFull,
+    text: 'Daylight Environment Forest Idyllic'
+  },
+  {
+    pre: FlightPre,
+    full: FlightFull,
+    text: 'Flight Landscape Nature Sky'
+  },
+  {
+    pre: GrayPre,
+    full: GrayFull,
+    text: 'Gray Bridge and Trees'
+  },
+  {
+    pre: GreenPre,
+    full: GreenFull,
+    text: 'Green Trees Under Blue and Orange Sky During Sunset'
+  }
+];
+
+const initState = {
+  showModal: false,
+  fullImage: '',
+  imageText: ''
+};
+
 function App() {
+  const [state, setState] = useState(initState);
+
+  // let previewsRef = React.useRef([]);
+  // if (previewsRef.length !== images.length) {
+  //   previewsRef = Array(images.length)
+  //     .fill()
+  //     .map((_, i) => previewsRef[i] || React.createRef());
+  // }
+
+  function handleModalOpen(item) {
+    setState((prevState) => ({
+      ...prevState,
+      showModal: true,
+      fullImage: item.full,
+      imageText: item.text
+    }));
+  }
+
+  function handleModalClose(event) {
+    if (event.target.classList.contains('modal')) {
+      setState((prevState) => ({
+        ...prevState,
+        showModal: false
+      }));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="gallery">
+        {images.map((item, index) => (
+          <div className="img-container" key={item.pre}>
+            <img
+              id={`image${index}`}
+              src={item.pre}
+              alt={item.text}
+              // ref={previewsRef[index]}
+              onClick={() => handleModalOpen(item)}
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        className={`modal ${state.showModal ? 'open' : ''}`}
+        onClick={handleModalClose}>
+        <img
+          src={state.fullImage}
+          alt="fullImage"
+          className={`full-img ${state.showModal ? 'open' : ''}`}
+        />
+        <p className="caption">{state.imageText}</p>
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/assets/images/index.js
@@ -0,0 +1,24 @@
+import CloudsPre from './preview/clouds-daylight-forest-grass-371589-preview.jpg';
+import DaylightPre from './preview/daylight-environment-forest-idyllic-459225-preview.jpg';
+import FlightPre from './preview/flight-landscape-nature-sky-36717-preview.jpg';
+import GrayPre from './preview/gray-bridge-and-trees-814499-preview.jpg';
+import GreenPre from './preview/green-trees-under-blue-and-orange-sky-during-sunset-1107717-preview.jpg';
+
+import CloudsFull from './full/clouds-daylight-forest-grass-371589-full.jpg';
+import DaylightFull from './full/daylight-environment-forest-idyllic-459225-full.jpg';
+import FlightFull from './full/flight-landscape-nature-sky-36717-full.jpg';
+import GrayFull from './full/gray-bridge-and-trees-814499-full.jpg';
+import GreenFull from './full/green-trees-under-blue-and-orange-sky-during-sunset-1107717-full.jpg';
+
+export {
+  CloudsPre,
+  DaylightPre,
+  FlightPre,
+  GrayPre,
+  GreenPre,
+  CloudsFull,
+  DaylightFull,
+  FlightFull,
+  GrayFull,
+  GreenFull
+};
